8e3866463382ed40bf6f67157d65b6d405fe0fdd
john <bblodfon@gmail.com>
2023-12-25 00:44:15 +0200

    remove bookdown files

--- build_book.sh
+++
@@ -1,5 +0,0 @@
-Rscript -e "
-Sys.setenv(RSTUDIO_PANDOC = '/usr/lib/rstudio/resources/app/bin/quarto/bin/tools'); 
-bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::gitbook')
-utils::browseURL(url = 'docs/index.html')
-"
